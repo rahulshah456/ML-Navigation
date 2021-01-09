8f414b37a833ae9606d8614637a5a7881cb1e75b
Rahul <rahulkumarshah5000@gmail.com>
2021-01-09 15:35:17 +0530

    dlib detections added

--- project.py
+++ project.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # ML Navigation Project
-# cross-platform application to perform the basic user navigation 
-# with face movement and hand gestures for easy accessibility, 
+# cross-platform application to perform the basic user navigation
+# with face movement and hand gestures for easy accessibility,
 # by using the inbuilt webcam for input
 #
 # Created by: @rahulshah456
@@ -10,13 +10,60 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication
 from PyQt5 import QtGui
 from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
+import imutils
+from imutils import face_utils
+from utils import *
+import pyautogui as pag
+import numpy as np
+import dlib
 import sys
 import cv2
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
-import numpy as np
+
+
+# Thresholds and consecutive frame length for triggering the mouse action.
+MOUTH_AR_THRESH = 0.6
+MOUTH_AR_CONSECUTIVE_FRAMES = 10
+EYE_AR_THRESH = 0.19
+EYE_AR_CONSECUTIVE_FRAMES = 10
+WINK_AR_DIFF_THRESH = 0.04
+WINK_AR_CLOSE_THRESH = 0.19
+WINK_CONSECUTIVE_FRAMES = 10
+
+
+# Initialize the frame counters for each action as well as
+# booleans used to indicate if action is performed or not
+MOUTH_COUNTER = 0
+EYE_COUNTER = 0
+WINK_COUNTER = 0
+INPUT_MODE = False
+EYE_CLICK = False
+LEFT_WINK = False
+RIGHT_WINK = False
+SCROLL_MODE = False
+ANCHOR_POINT = (0, 0)
+WHITE_COLOR = (255, 255, 255)
+YELLOW_COLOR = (0, 255, 255)
+GREEN_COLOR = (0, 255, 0)
+BLUE_COLOR = (255, 0, 0)
+BLACK_COLOR = (0, 0, 0)
+
+
+# Init Dlib's face detectors and predictors
+shape_predictor = "models/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(shape_predictor)
+
+
+# Initial the indexes of the facial landmarks for the left and
+# right eye, nose and mouth respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
 
 
 class Thread(QThread):
@@ -31,16 +78,149 @@ class Thread(QThread):
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 h, w, ch = rgbImage.shape
                 bytesPerLine = ch * w
-                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                convertToQtFormat = QImage(
+                    rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
                 p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
                 self.changePixmap.emit(p)
+
+
+                # Detect faces in the grayscale frame
+                rects = detector(rgbImage, 0)
+
+                # Loop over the face detections
+                if len(rects) > 0:
+                    rect = rects[0]
+
+                    # Determine the facial landmarks for the face region, then
+                    # convert the facial landmark (x, y)-coordinates to a NumPy
+                    # array
+                    shape = predictor(frame, rect)
+                    shape = face_utils.shape_to_np(shape)
+
+                    # Extract the left and right eye coordinates, then use the
+                    # coordinates to compute the eye aspect ratio for both eyes
+                    mouth = shape[mStart:mEnd]
+                    leftEye = shape[lStart:lEnd]
+                    rightEye = shape[rStart:rEnd]
+                    nose = shape[nStart:nEnd]
+
+                    # Because I flipped the frame, left is right, right is left.
+                    temp = leftEye
+                    leftEye = rightEye
+                    rightEye = temp
+
+                    # Average the mouth aspect ratio together for both eyes
+                    mar = mouth_aspect_ratio(mouth)
+                    leftEAR = eye_aspect_ratio(leftEye)
+                    rightEAR = eye_aspect_ratio(rightEye)
+                    ear = (leftEAR + rightEAR) / 2.0
+                    diff_ear = np.abs(leftEAR - rightEAR)
+
+                    nose_point = (nose[3, 0], nose[3, 1])
+
+                    # Compute the convex hull for the left and right eye, then
+                    # visualize each of the eyes
+                    mouthHull = cv2.convexHull(mouth)
+                    leftEyeHull = cv2.convexHull(leftEye)
+                    rightEyeHull = cv2.convexHull(rightEye)
+                    cv2.drawContours(frame, [mouthHull], -1, YELLOW_COLOR, 1)
+                    cv2.drawContours(frame, [leftEyeHull], -1, YELLOW_COLOR, 1)
+                    cv2.drawContours(frame, [rightEyeHull], -1, YELLOW_COLOR, 1)
+
+                    for (x, y) in np.concatenate((mouth, leftEye, rightEye), axis=0):
+                        cv2.circle(frame, (x, y), 2, GREEN_COLOR, -1)
+
+                    # Check to see if the eye aspect ratio is below the blink
+                    # threshold, and if so, increment the blink frame counter
+                    if diff_ear > WINK_AR_DIFF_THRESH:
+
+                        if leftEAR < rightEAR:
+                            if leftEAR < EYE_AR_THRESH:
+                                WINK_COUNTER += 1
+
+                                if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+                                    pag.click(button='left')
+
+                                    WINK_COUNTER = 0
+
+                        elif leftEAR > rightEAR:
+                            if rightEAR < EYE_AR_THRESH:
+                                WINK_COUNTER += 1
+
+                                if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+                                    pag.click(button='right')
+
+                                    WINK_COUNTER = 0
+                        else:
+                            WINK_COUNTER = 0
+                    else:
+                        if ear <= EYE_AR_THRESH:
+                            EYE_COUNTER += 1
+
+                            if EYE_COUNTER > EYE_AR_CONSECUTIVE_FRAMES:
+                                SCROLL_MODE = not SCROLL_MODE
+                                # INPUT_MODE = not INPUT_MODE
+                                EYE_COUNTER = 0
+
+                                # nose point to draw a bounding box around it
+
+                        else:
+                            EYE_COUNTER = 0
+                            WINK_COUNTER = 0
+
+                    if mar > MOUTH_AR_THRESH:
+                        MOUTH_COUNTER += 1
+
+                        if MOUTH_COUNTER >= MOUTH_AR_CONSECUTIVE_FRAMES:
+                            # if the alarm is not on, turn it on
+                            INPUT_MODE = not INPUT_MODE
+                            # SCROLL_MODE = not SCROLL_MODE
+                            MOUTH_COUNTER = 0
+                            ANCHOR_POINT = nose_point
+
+                    else:
+                        MOUTH_COUNTER = 0
+
+                    # if INPUT_MODE:
+                    #     cv2.putText(frame, "INPUT MODE", (10, 30),
+                    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, BLUE_COLOR, 2)
+                    #     x, y = ANCHOR_POINT
+                    #     nx, ny = nose_point
+                    #     w, h = 60, 35
+                    #     multiple = 1
+                    #     cv2.rectangle(frame, (x - w, y - h),
+                    #                 (x + w, y + h), GREEN_COLOR, 2)
+                    #     cv2.line(frame, ANCHOR_POINT, nose_point, BLUE_COLOR, 2)
+
+                    #     dir = direction(nose_point, ANCHOR_POINT, w, h)
+                    #     cv2.putText(frame, dir.upper(), (10, 90),
+                    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, BLUE_COLOR, 2)
+                    #     drag = 18
+                    #     if dir == 'right':
+                    #         pag.moveRel(drag, 0)
+                    #     elif dir == 'left':
+                    #         pag.moveRel(-drag, 0)
+                    #     elif dir == 'up':
+                    #         if SCROLL_MODE:
+                    #             pag.scroll(40)
+                    #         else:
+                    #             pag.moveRel(0, -drag)
+                    #     elif dir == 'down':
+                    #         if SCROLL_MODE:
+                    #             pag.scroll(-40)
+                    #         else:
+                    #             pag.moveRel(0, drag)
+
+                    # if SCROLL_MODE:
+                    #     cv2.putText(frame, 'SCROLL MODE IS ON!', (10, 60),
+                    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, BLUE_COLOR, 2)    
 
 
 class Ui_MainWindow(QWidget):
 
     def setupUi(self, MainWindow):
 
-        # home page of the main application   
+        # home page of the main application
         MainWindow.setObjectName("MainWindow")
         MainWindow.setFixedSize(640, 480)
         MainWindow.setFocusPolicy(QtCore.Qt.StrongFocus)
@@ -63,7 +243,7 @@ class Ui_MainWindow(QWidget):
         font.setWeight(75)
         self.label.setFont(font)
         self.label.setStyleSheet("color: rgb(255, 255, 255);"
-                                "margin:12px")
+                                 "margin:12px")
         self.label.setWordWrap(True)
         self.label.setObjectName("label")
 
@@ -143,9 +323,6 @@ class Ui_MainWindow(QWidget):
         self.videoFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.videoFrame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.videoFrame.setObjectName("videoFrame")
-        th = Thread(MainWindow)
-        th.changePixmap.connect(self.setImage)
-        th.start()
 
         # checkboxes menu to activate and deactivate features
         self.label_2 = QtWidgets.QLabel(MainWindow)
@@ -219,15 +396,15 @@ class Ui_MainWindow(QWidget):
         self.saveButton.setObjectName("saveButton")
         self.horizontalLayout.addWidget(self.saveButton)
 
+        #subscribe the threads
+        th = Thread(MainWindow)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+
         # add widgets texts with translation support
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    @pyqtSlot(QImage)
-    def setImage(self, image):
-        # update video frame as image to the window
-        self.videoFrame.setPixmap(QPixmap.fromImage(image))
-    
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "ML Navigation"))
@@ -253,6 +430,12 @@ class Ui_MainWindow(QWidget):
         self.checkBox_4.setText(_translate("MainWindow", "Mouth Open to Undo"))
         self.saveButton_2.setText(_translate("MainWindow", "Detect Camera"))
         self.saveButton.setText(_translate("MainWindow", "Enable / Save"))
+
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        # update video frame as image to the window
+        self.videoFrame.setPixmap(QPixmap.fromImage(image))
+            
 
 
 if __name__ == "__main__":
